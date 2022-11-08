4a43bfc492995870e1f28f9cb5c49e7bd379f1e4
UmarSauqi8 <68529348+UmarSauqi8@users.noreply.github.com>
2022-11-08 11:36:19 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Dosen dosen1 = new Dosen(0137, "Umar", "bengkulu, Yogyakarta", 05,05,2003, "Laki-Laki", 2016, 2100018137, "Informatika",8 );
+        Dosen dosen1 = new Dosen(0137, "Umar", "Bengkulu, Bengkulu", 05,05,2003, "Laki-Laki", 2016, 2100018137, "Informatika",10 );
+        System.out.println("Dosen");
         dosen1.cetak();
 
-        Tendik tendik1 = new Tendik(1231,"sauqi", "bumi ijo, Yogyakarta",01,01,2001,"Laki-Laki", 2020, 10);
+        Tendik tendik1 = new Tendik(0138,"sauqi", "Bumi ijo, Yogyakarta",01,01,2001,"Laki-Laki", 2020, 10);
+        System.out.println("Tendik");
         tendik1.cetak();
     }
 }
